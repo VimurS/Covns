ae781b085f5c7a69834abaff3e6b81288b148d05
Vimur <vimurs07@gmail.com>
2023-07-22 00:22:45 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import Contact from './Pages/contact/contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import brandLogo from "./img/brandlogo.png";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
 
 function App() {
   return (
-<BrowserRouter>
-<Routes>
-  <Route index path='/Covns' element={<LandingPage />}></Route>
-  <Route path='/home' element={<LandingPage />}></Route>
-  <Route path='/aboutus' element={<Aboutus />}></Route>
-</Routes>
-    
-</BrowserRouter>
- 
+    <HashRouter>
+      <Routes>
+        <Route index path='/Covns' element={<LandingPage />}></Route>
+        <Route path='/home' element={<LandingPage />}></Route>
+        <Route path='/aboutus' element={<Aboutus />}></Route>
+      </Routes>
+
+    </HashRouter>
+
   );
 }
 
