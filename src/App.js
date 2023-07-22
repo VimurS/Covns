f599d6b083b50dc3de4a872486b50c6df33ef5d2
Vimur <vimurs07@gmail.com>
2023-07-22 19:02:37 +0530

    routes fxing try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import LandingPage from './Pages/landingPage/landingPage';
+// import Aboutus from "./Pages/aboutus/aboutus";
+// import Products from './Pages/Products/Products';
+// import Contact from './Pages/contact/contact';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import brandLogo from "./img/brandlogo.png";
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//  import { HashRouter } from 'react-router-dom'
+
+
+// function App() {
+//   return (
+// <BrowserRouter>
+// <Routes>
+//   <Route index path='/Covns' element={<LandingPage />}></Route>
+//   <Route path='/home' element={<LandingPage />}></Route>
+//   <Route path='/aboutus' element={<Aboutus />}></Route>
+//   <Route path='/products' element={<Products />}></Route>
+// </Routes>
+    
+// </BrowserRouter>
+ 
+//   );
+// }
+
+// export default App;
+
+// App.js
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/landingPage/landingPage';
-import Aboutus from "./Pages/aboutus/aboutus";
+import Aboutus from './Pages/aboutus/aboutus';
 import Products from './Pages/Products/Products';
 import Contact from './Pages/contact/contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import brandLogo from "./img/brandlogo.png";
+import brandLogo from './img/brandlogo.png';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/home' element={<LandingPage />} />
-        <Route path='/aboutus' element={<Aboutus />} />
-        <Route path='/products' element={<Products />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<LandingPage />} />
+      <Route path="/aboutus" element={<Aboutus />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+// import LandingPage from './Pages/landingPage/landingPage';
+// import Aboutus from "./Pages/aboutus/aboutus";
+// import Products from './Pages/Products/Products';
+// import Contact from './Pages/contact/contact';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import brandLogo from "./img/brandlogo.png";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path='/' element={<LandingPage />} />
+//         <Route path='/home' element={<LandingPage />} />
+//         <Route path='/aboutus' element={<Aboutus />} />
+//         <Route path='/products' element={<Products />} />
+//         {/* Add more routes as needed */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter } from 'react-router-dom';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+//     <App />
+//     {/* </BrowserRouter> */}
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-   {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+ReactDOM.render(
+  <Router>
     <App />
-    {/* </BrowserRouter> */}
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
-
-reportWebVitals();
