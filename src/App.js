11eab5602310ab100b780cb4f3ca476b4871d453
Vimur <vimurs07@gmail.com>
2023-07-22 18:43:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/landingPage/landingPage';
 import Aboutus from "./Pages/aboutus/aboutus";
 import Products from './Pages/Products/Products';
 import Contact from './Pages/contact/contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import brandLogo from "./img/brandlogo.png";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
- import { HashRouter } from 'react-router-dom'
-
 
 function App() {
   return (
-<HashRouter>
-<Routes>
-  <Route index path='/Covns' element={<LandingPage />}></Route>
-  <Route path='/home' element={<LandingPage />}></Route>
-  <Route path='/aboutus' element={<Aboutus />}></Route>
-  <Route path='/products' element={<Products />}></Route>
-</Routes>
-    
-</HashRouter>
- 
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/home' element={<LandingPage />} />
+        <Route path='/aboutus' element={<Aboutus />} />
+        <Route path='/products' element={<Products />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
